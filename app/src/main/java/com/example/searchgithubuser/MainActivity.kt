e5da64eb53e7d728703eca065554cfdb64e6aa15
taro <ichi9ta@gmail.com>
2022-01-03 21:31:30 +0900

    modify TopBar title.

--- app/src/main/java/com/example/searchgithubuser/MainActivity.kt
+++ app/src/main/java/com/example/searchgithubuser/MainActivity.kt
@@ -1,29 +1,8 @@
 package com.example.searchgithubuser
 
-import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.browser.customtabs.CustomTabsIntent
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.example.searchgithubuser.SearchGitHubUserApplication.Companion.appContext
-import com.example.searchgithubuser.ui.SearchUsersScreen
-import com.example.searchgithubuser.ui.SearchUsersViewModel
-import com.example.searchgithubuser.ui.UserInfoScreen
-import com.example.searchgithubuser.ui.theme.SearchGithubUsersTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -32,66 +11,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SearchGitHubUserApp()
-        }
-    }
-}
-
-private fun launchRepositoryPage(url: String) {
-    val builder = CustomTabsIntent.Builder()
-    val customTabsIntent = builder.build()
-    customTabsIntent.intent.flags = FLAG_ACTIVITY_NEW_TASK
-    customTabsIntent.launchUrl(appContext, Uri.parse(url))
-}
-
-@Composable
-fun SearchGitHubUserApp() {
-    SearchGithubUsersTheme {
-        val navController = rememberNavController()
-        val backstackEntry = navController.currentBackStackEntryAsState()
-        val currentScreen = SearchGitHubUsersScreen.fromRoute(backstackEntry.value?.destination?.route)
-
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text(currentScreen.name) }
-                )
-            }
-        ) {
-            SearchGitHubUsersNavHost(navController, modifier = Modifier.padding(it))
-        }
-    }
-}
-
-@Composable
-fun SearchGitHubUsersNavHost(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
-) {
-    NavHost(
-        navController = navController,
-        startDestination = SearchGitHubUsersScreen.Search.name,
-        modifier = modifier
-    ) {
-        composable(SearchGitHubUsersScreen.Search.name) {
-            SearchUsersScreen(
-                onClickUserList = {
-                    navController.navigate(SearchGitHubUsersScreen.UserInfo.name)
-                }
-            )
-        }
-        composable(SearchGitHubUsersScreen.UserInfo.name) {
-            val parentEntry = remember {
-                navController.getBackStackEntry(SearchGitHubUsersScreen.Search.name)
-            }
-            val viewModel = hiltViewModel<SearchUsersViewModel>(
-                parentEntry
-            )
-            UserInfoScreen(
-                viewModel = viewModel,
-                onClickRepositoryList = { repositoryUrl ->
-                    launchRepositoryPage(repositoryUrl)
-                }
-            )
         }
     }
 }
